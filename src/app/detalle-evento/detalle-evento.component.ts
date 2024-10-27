9ecd90f25a979cbad61b086f61c4dc86c23e21b2
Guillermo <guillermo.garcia-fonseca.external@capgemini.com>
2024-10-27 21:13:20 +0100

    Inicio terminado

--- src/app/detalle-evento/detalle-evento.component.ts
+++ src/app/detalle-evento/detalle-evento.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-detalle-evento',
   standalone: true,
---
+++ src/app/model/evento.model.ts
@@ -0,0 +1,11 @@
+export class Evento {
+    id_evento?: number;
+    nombre!: string;
+    descripcion!: string;
+    genero!: string;
+    fecha_evento!: Date;
+    precio_min!: number;
+    precio_max!: number;
+    localidad!: string;
+    recinto!: string;
+  }
