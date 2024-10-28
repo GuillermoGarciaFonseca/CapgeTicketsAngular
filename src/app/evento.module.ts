60c2b98283f11f326a811874f942be5fdd5c31c5
Gregorio Rosas <laura.gregorio-rosas@capgemini.com>
2024-10-28 08:44:47 +0100

    added imports

--- src/app/evento.module.ts
+++ src/app/evento.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HttpClientModule, HttpClient } from '@angular/common/http'; 
+import { FormsModule } from '@angular/forms';
+import { EventoService } from './service/evento.service';
 import { EventoRoutingModule } from './evento-routing.module';
-
+import { DarAltaComponent } from './dar-alta/dar-alta.component';
+import { DetalleEventoComponent } from './detalle-evento/detalle-evento.component';
+import { ListaEventosComponent } from './lista-eventos/lista-eventos.component';
+import { ModificarEventosComponent } from './modificar-eventos/modificar-eventos.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    EventoRoutingModule
-  ]
+    EventoRoutingModule,
+    HttpClientModule,
+    FormsModule
+    
+  ],
+  providers: [HttpClient, EventoService],
+  bootstrap: [AppComponent]
 })
 export class EventoModule { }
